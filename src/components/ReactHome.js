d9b56342a959754869811e22a0ff47f96c9e0c4a
Sean Mortimer <sean.mortimer@gmail.com>
2021-12-16 12:27:50 -0700

    Test commit

--- src/components/ReactHome.js
+++ src/components/ReactHome.js
@@ -8,7 +8,7 @@ class ReactHome extends Component {
         <h1>Welcome to React!</h1>
         <h5>Click on a spinning icon to navigate the app</h5>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>And most importantly, have fun!</p>
+        <p>And most importantly, have fun!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
